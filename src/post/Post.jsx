4338c32d2115da1e3bbf800bf9630fe4b669ed92
Tim Meermans <timmeermans@live.nl>
2021-07-18 09:54:51 +0200

    Homepage body now filled with dummy posts

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -3,7 +3,27 @@ import "./post.css"
 export default function Post() {
     return (
         <div className="post">
-            posts
+            <img className="postImg" src="https://images.pexels.com/photos/3761196/pexels-photo-3761196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            <div className="postInfo">
+                <div className="postCats">
+                    <span className="postCat">Music</span>
+                    <span className="postCat">Life</span>
+                </div>
+                <span className="postTitle">Lorem ipsum dolor sit amet</span>
+                <hr />
+                <span className="postDate">1 hour ago</span>
+            </div>
+            <p className="postDesc">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Architecto quasi maxime nam modi, laboriosam iste cumque voluptatem perspiciatis atque delectus, 
+                labore illo consequatur eos tempora distinctio perferendis. Sunt, consectetur error.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Architecto quasi maxime nam modi, laboriosam iste cumque voluptatem perspiciatis atque delectus, 
+                labore illo consequatur eos tempora distinctio perferendis. Sunt, consectetur error.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Architecto quasi maxime nam modi, laboriosam iste cumque voluptatem perspiciatis atque delectus, 
+                labore illo consequatur eos tempora distinctio perferendis. Sunt, consectetur error.
+            </p>
         </div>
     )
 }

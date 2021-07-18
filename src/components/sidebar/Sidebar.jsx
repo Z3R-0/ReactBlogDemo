c9af1c256d52eee7b50b226cf1e6f6df7b6458bb
Tim Meermans <timmeermans@live.nl>
2021-07-18 13:15:04 +0200

    Rearranged folder structure and added more pages

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -9,7 +9,7 @@ export default function Sidebar() {
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, sed. Eos exercitationem repudiandae possimus fuga magnam unde, labore ea.</p>
             </div>
             <div className="sidebarItem">
-                <span className="sideBarTitle">CATEGORIES</span>
+                <span className="sidebarTitle">CATEGORIES</span>
                 <ul className="sidebarList">
                     <li className="sidebarListItem">Life</li>
                     <li className="sidebarListItem">Music</li>
@@ -20,7 +20,7 @@ export default function Sidebar() {
                 </ul>
             </div>
             <div className="sidebarItem">
-                <span className="sidebareTitle">FOLLOW US</span>
+                <span className="sidebarTitle">FOLLOW US</span>
                 <div className="sidebarSocial">
                     <i className="sidebarIcon fab fa-facebook-square"></i>
                     <i className="sidebarIcon fab fa-twitter-square"></i>
---
+++ src/components/singlePost/SinglePost.jsx
@@ -0,0 +1,56 @@
+import "./singlePost.css"
+
+export default function SinglePost() {
+    return (
+        <div className="singlePost">
+            <div className="singlePostWrapper">
+                <img src="https://images.pexels.com/photos/261662/pexels-photo-261662.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="singlePostImg" />
+                <h1 className="singlePostTitle">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    <div className="singlePostEdit">
+                        <i className="singlePostIcon far fa-edit"></i>
+                        <i className="singlePostIcon far fa-trash-alt"></i>
+                    </div>
+                </h1>
+                <div className="singlePostInfo">
+                    <span className="singlePostAuthor">Author: <b>Resh</b></span>
+                    <span className="singlePostDate">1 hour ago</span>
+                </div>
+                <p className="singlePostDesc">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
+                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
+                        error dignissimos porro labore.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
+                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
+                        error dignissimos porro labore.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
+                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
+                        error dignissimos porro labore.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
+                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
+                        error dignissimos porro labore.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
+                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
+                        error dignissimos porro labore.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
+                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
+                        error dignissimos porro labore.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
+                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
+                        error dignissimos porro labore.
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
+                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
+                        error dignissimos porro labore.
+                    </p>
+            </div>
+        </div>
+    )
+}

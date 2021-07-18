7f4769ffef0180ea6fa3c45e78bca4915507876f
Tim Meermans <timmeermans@live.nl>
2021-07-18 08:39:14 +0200

    Sidebar setup

---
+++ src/sidebar/Sidebar.jsx
@@ -0,0 +1,33 @@
+import "./sidebar.css"
+
+export default function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebarItem">
+                <span className="sidebarTitle">ABOUT ME</span>
+                <img src="https://images.pexels.com/photos/276452/pexels-photo-276452.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, sed. Eos exercitationem repudiandae possimus fuga magnam unde, labore ea.</p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sideBarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Life</li>
+                    <li className="sidebarListItem">Music</li>
+                    <li className="sidebarListItem">Style</li>
+                    <li className="sidebarListItem">Sport</li>
+                    <li className="sidebarListItem">Tech</li>
+                    <li className="sidebarListItem">Cinema</li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebareTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fab fa-facebook-square"></i>
+                    <i className="sidebarIcon fab fa-twitter-square"></i>
+                    <i className="sidebarIcon fab fa-pinterest-square"></i>
+                    <i className="sidebarIcon fab fa-instagram-square"></i>
+                </div>
+            </div>
+        </div>
+    )
+}

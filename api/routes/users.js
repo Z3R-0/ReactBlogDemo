17958668e3a9b72ecc4d9f15bdaee5e302c98a5a
Tim Meermans <timmeermans@live.nl>
2021-07-21 19:22:18 +0200

    Added delete and update functions

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 // Update
@@ -12,7 +13,7 @@ router.put("/:id", async(req, res) => {
         try {
             const updatedUser = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
-            });
+            }, { new: true });
             res.status(200).json(updatedUser);
         } catch (err) {
             res.status(500).json(err);
@@ -23,5 +24,26 @@ router.put("/:id", async(req, res) => {
 });
 
 // Delete
+router.delete("/:id", async(req, res) => {
+    if (req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id);
+            if (!user) {
+                throw "User not found";
+            }
+            try {
+                await Post.deleteMany({ username: user.username });
+                await User.findByIdAndDelete(req.params.id);
+                res.status(200).json("User has been deleted...");
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        } catch (err) {
+            res.status(404).json("User not found");
+        }
+    } else {
+        res.status(401).json("You can only delete your own account");
+    }
+});
 
 module.exports = router;

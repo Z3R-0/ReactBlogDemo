946463340a06a4dd3e813745a62a6e95aeb4d046
Tim Meermans <timmeermans@live.nl>
2021-07-22 23:34:32 +0200

    Finished api side for now (posts, categories and images)

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -58,13 +58,37 @@ router.delete("/:id", async(req, res) => {
 // Get post
 router.get("/:id", async(req, res) => {
     try {
-        const user = await User.findById(req.params.id);
-        const { password, ...others } = user._doc;
+        const post = await Post.findById(req.params.id);
 
-        res.status(200).json(others);
+        res.status(200).json(post);
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+// Get all posts
+router.get("/", async(req, res) => {
+    const username = req.query.user;
+    const catName = req.query.cat;
+    try {
+        let posts;
+
+        if (username) {
+            posts = await Post.find({ username })
+        } else if (catName) {
+            posts = await Post.find({
+                categories: {
+                    $in: [catName]
+                }
+            });
+        } else {
+            posts = await Post.find();
+        }
+
+        res.status(200).json(posts);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;

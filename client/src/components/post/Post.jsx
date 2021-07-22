b9fafa37b3e80db714eb5cc67e2aee1165c715c0
Tim Meermans <timmeermans@live.nl>
2021-07-23 00:16:24 +0200

    Binding api to client

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,32 +1,24 @@
 import { Link } from "react-router-dom"
 import "./post.css"
 
-export default function Post() {
+export default function Post({post}) {
     return (
         <div className="post">
-            <img className="postImg" src="https://images.pexels.com/photos/3761196/pexels-photo-3761196.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
+            {post.photo && ( 
+                <img className="postImg" src={post.photo} alt="" />
+            )}
+
             <div className="postInfo">
                 <div className="postCats">
-                    <span className="postCat">Music</span>
-                    <span className="postCat">Life</span>
+                    {post.categories.map((c)=>(
+                        <span className="postCat">{c.name}</span>
+                    ))}
                 </div>
-                <span className="postTitle">
-                    <Link className="link" to="/post/:1">Lorem ipsum dolor sit amet</Link>
-                </span>
+                <span className="postTitle">{post.title}</span>
                 <hr />
-                <span className="postDate">1 hour ago</span>
+                <span className="postDate">{new Date(post.createdAt).toDateString()}</span>
             </div>
-            <p className="postDesc">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Architecto quasi maxime nam modi, laboriosam iste cumque voluptatem perspiciatis atque delectus, 
-                labore illo consequatur eos tempora distinctio perferendis. Sunt, consectetur error.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Architecto quasi maxime nam modi, laboriosam iste cumque voluptatem perspiciatis atque delectus, 
-                labore illo consequatur eos tempora distinctio perferendis. Sunt, consectetur error.
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Architecto quasi maxime nam modi, laboriosam iste cumque voluptatem perspiciatis atque delectus, 
-                labore illo consequatur eos tempora distinctio perferendis. Sunt, consectetur error.
-            </p>
+            <p className="postDesc">{post.desc}</p>
         </div>
     )
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,15 +1,26 @@
+import { useEffect, useState } from "react"
 import Header from "../../components/header/Header"
 import Posts from "../../components/posts/Posts"
 import Sidebar from "../../components/sidebar/Sidebar"
 import "./home.css"
+import axios from "axios"
 
 export default function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async ()=>{
+            const res = await axios.get("/posts");
+            setPosts(res.data);
+        }
+        fetchPosts();
+    }, [])
     return (
         <>
-            <Header/>
+            <Header />
             <div className="home">
-                <Posts/>
-                <Sidebar/>
+                <Posts posts={posts} />
+                <Sidebar />
             </div>
         </>
     )

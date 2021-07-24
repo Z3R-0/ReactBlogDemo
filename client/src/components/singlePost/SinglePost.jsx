d564db530ebaafee770e1047653df1a22795ad40
Tim Meermans <timmeermans@live.nl>
2021-07-25 00:02:52 +0200

    Further connecting of api to client

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,55 +1,45 @@
+import axios from 'axios'
+import { Link, useLocation } from "react-router-dom"
+import { useEffect, useState } from "react"
 import "./singlePost.css"
 
 export default function SinglePost() {
+    const location = useLocation();
+    const path = location.pathname.split("/")[2];
+    const [post, setPost] = useState({});
+
+    useEffect(() => {
+        const getPost = async () => {
+            const res = await axios.get("/posts/" + path);
+            setPost(res.data);
+        }
+        getPost();
+    }, [path]);
+
     return (
         <div className="singlePost">
             <div className="singlePostWrapper">
-                <img src="https://images.pexels.com/photos/261662/pexels-photo-261662.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" className="singlePostImg" />
+                {post.img &&
+                    <img src={post.photo}
+                        alt=""
+                        className="singlePostImg" />
+                }
                 <h1 className="singlePostTitle">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    {post.title}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon far fa-edit"></i>
                         <i className="singlePostIcon far fa-trash-alt"></i>
                     </div>
                 </h1>
                 <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <b>Resh</b></span>
-                    <span className="singlePostDate">1 hour ago</span>
+                    <span className="singlePostAuthor">Author: 
+                    <Link to={`/?user=${post.username}`} className="link">
+                        <b>{post.username}</b>
+                    </Link>
+                    </span>
+                    <span className="singlePostDate">{new Date(post.createdAt).toDateString()}</span>
                 </div>
-                <p className="singlePostDesc">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
-                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
-                        error dignissimos porro labore.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
-                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
-                        error dignissimos porro labore.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
-                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
-                        error dignissimos porro labore.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
-                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
-                        error dignissimos porro labore.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
-                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
-                        error dignissimos porro labore.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
-                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
-                        error dignissimos porro labore.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
-                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
-                        error dignissimos porro labore.
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                        Provident asperiores harum saepe mollitia nam, eius sint ex ullam, 
-                        ducimus perferendis labore? Nobis natus ratione quisquam facere 
-                        error dignissimos porro labore.
-                    </p>
+                <p className="singlePostDesc">{post.desc}</p>
             </div>
         </div>
     )
